2e06d294bef1dab5ef578f5a9d5d01e9c9772a2e
Solomon <solomon4eg@gmail.com>
2013-02-27 23:00:35 +0400

    links

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ActiveRecord::Base
   belongs_to :client
-  has_many :tables ,:through =>  :rel_table_orders ,:source =>  "table_id"
+  has_many :tables ,:through =>  :rel_table_orders,:source =>  :table
   has_many :rel_table_orders
+
 end
